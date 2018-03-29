6c6b7a4b7cc3da20ad4116779ec0ddf43b61b111
ShiroTakeda <shiro.takeda@gmail.com>
2018-03-29 13:46:34 +0900

    Added explanatory text.

--- gams-ac.el
+++ gams-ac.el
@@ -3,11 +3,11 @@
 ;; Author: Shiro Takeda
 ;; Maintainer: Shiro Takeda
 ;; Copyright (C) 2018 Shiro Takeda
-;; First Created: Tue Jan 23, 2018 
-;; Time-stamp: <2018-03-27 15:34:21 st>
-;; Version: 0.9
-;; Keywords: GAMS, auto-complete
-;; URL: http://shirotakeda.org/en/gams/gams-mode/
+;; First Created: Tue Jan 23, 2018
+;; Time-stamp: <2018-03-29 13:46:11 st>
+;; Version: 1.0
+;; Keywords: GAMS, gams-mode, auto-complete
+;; URL: https://github.com/ShiroTakeda/gams-ac
 ;; This file is not part of any Emacs.
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -50,33 +50,38 @@
 (require 'auto-complete)
 (require 'gams-mode)
 
-(defconst gams-ac-version "0.9"
+(defconst gams-ac-version "1.0"
   "Version of `gams-ac.el'.")
 
 (defvar gams-ac-source-user-keywords-list nil
   "A list of user keywords.")
 
-(defvar gams-ac-sources
+;; A variable of auto-complete source for GAMS mode.
+(setq gams-ac-sources
   '(gams-ac-source-user-keywords
     gams-ac-source-basic-commands
     gams-ac-source-dollar-control
     ))
 
 (defun gams-ac-setup ()
+  "Set up `auto-complete' for GAMS mode."
   (setq ac-sources (append gams-ac-sources ac-sources)))
 
 (defun gams-ac-after-init-setup ()
+  "A function that should be executed in the init file."
   (add-to-list 'ac-modes 'gams-mode)
   (add-hook 'gams-mode-hook 'gams-ac-setup)
   )
 
 ;; keywords
 (defmacro gams-ac-define-dictionary-source (name list)
+  "Macro for creating `auto-complete' sources NAME from LIST."
   `(defconst ,name
      '((candidates . (lambda () (all-completions ac-prefix ,list)))
        )))
 
 (defsubst gams-ac-attach-$ (list)
+  "Macro for attaching $ mark to each element of LIST."
   (mapcar #'(lambda (x) (concat "$" x)) list))
 
 ;; user keywords (command, option or varoable)
@@ -95,12 +100,8 @@
  (gams-ac-attach-$
   (gams-alist-to-list gams-dollar-control-alist)))
 
+
 (provide 'gams-ac)
 
-
 ;;; gams-ac.el ends here.
-
-;; Local Variables:
-;; coding: utf-8
-;; End:
 
